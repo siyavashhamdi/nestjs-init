4b613ae503a915f044d0338e858f1e6e5445af18
Siyavash <siyavash.hamdi@curogram.com>
2021-03-28 22:44:39 +0430

    Fastify only

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,12 @@
-import { Get, Controller, Render } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-    constructor(private readonly appService: AppService) { }
+  constructor(private readonly appService: AppService) {}
 
-    @Get()
-    @Render('index')
-    render() {
-        const message = this.appService.getHello();
-        return { message };
-    }
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
 }
